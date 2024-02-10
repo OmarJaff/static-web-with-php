f9ccd1dfb4434fc711f0cf06f930e16c00050257
OmarJaff <me@omarjaff.com>
2024-02-10 17:12:11 +0300

    playing with PDO class and db connections with php

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 
 require 'functions.php';
 
-require 'routes.php';
+//require 'routes.php';
+
+
+$db = new Database();
+
+$posts = $db->query("select * from posts where id > 1")->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
+
+//foreach ($posts as $post) {
+//    echo "<li>" . $post['title'] ."</li>";
+//}
+
+
 
 
