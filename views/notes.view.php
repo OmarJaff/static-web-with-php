cdacdfd7282647222ba841a0972c63c99d8b5613
OmarJaff <me@omarjaff.com>
2024-02-11 23:05:51 +0300

    fixed

--- views/notes.view.php
+++ views/notes.view.php
@@ -1,13 +1,18 @@
 <?php require ('partials/head.php') ?>
 
 <?php require 'partials/nav.php' ?>
+
 <?php require 'partials/banner.php' ?>
+
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 mt-6">
              <ul class="text-xl disc space-y-3">
-                 <?php foreach($notes as $note)  :?>
+                 <?php foreach($notes as $note)  : ?>
+
                  <li>
+                     <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 underline line-clamp-1">
                      <?= $note['body'] ?>
+                     </a>
                  </li>
                     <?php endforeach; ?>
              </ul>
