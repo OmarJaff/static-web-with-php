d5bd45a35730b9c19214fa301279b74616f4d29f
OmarJaff <me@omarjaff.com>
2024-02-11 23:06:10 +0300

    Added note contoller and its logic

--- controllers/note.php
+++ controllers/note.php
@@ -1,1 +1,14 @@
 <?php
+
+$heading = "My Note";
+$config = require ('config.php');
+
+
+
+$db = new Database($config['database']);
+
+
+$note = $db->query('select * from notes where id = :id', [':id' => $_GET['id']])->fetch();
+
+require 'views/note.view.php';
+
