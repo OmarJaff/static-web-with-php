8079b864101108e6bc562291798d7f9d646a10b0
OmarJaff <me@omarjaff.com>
2024-02-11 22:14:29 +0300

    added router to notes page

--- routes.php
+++ routes.php
@@ -6,6 +6,7 @@ $url = parse_url($_SERVER['REQUEST_URI'])['path'];
 $routes = [
     '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
+    '/notes' => 'controllers/notes.php',
     '/contact' => 'controllers/contact.php'
 ];
 
