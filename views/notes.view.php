8de1546ae07f4beb96966e58d8ac90026f8a49e4
OmarJaff <me@omarjaff.com>
2024-02-11 22:22:55 +0300

    Added html codes for the notes

--- views/notes.view.php
+++ views/notes.view.php
@@ -1,1 +1,18 @@
-<?php
+<?php require ('partials/head.php') ?>
+
+<?php require 'partials/nav.php' ?>
+<?php require 'partials/banner.php' ?>
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 mt-6">
+             <ul class="text-xl disc space-y-3">
+                 <?php foreach($notes as $note)  :?>
+                 <li>
+                     <?= $note['body'] ?>
+                 </li>
+                    <?php endforeach; ?>
+             </ul>
+        </div>
+    </main>
+
+
+<?php require ('partials/footer.php') ?>
