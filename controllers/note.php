acc08b6734c6647af8d15d3c5319f337a32f2929
OmarJaff <me@omarjaff.com>
2024-02-11 23:38:12 +0300

    simple refactoring

--- controllers/note.php
+++ controllers/note.php
@@ -10,5 +10,14 @@ $db = new Database($config['database']);
 
 $note = $db->query('select * from notes where id = :id', [':id' => $_GET['id']])->fetch();
 
+if (!$note)
+{
+    abort();
+}
+
+if ($note['user_id'] !== 1) {
+    abort(Response::NOT_FOUND);
+}
+
 require 'views/note.view.php';
 
