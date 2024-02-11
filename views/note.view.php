9aba96295d1264ff465280db12071d59862f25e0
OmarJaff <me@omarjaff.com>
2024-02-11 23:06:47 +0300

    Added particuler note view page

--- views/note.view.php
+++ views/note.view.php
@@ -1,1 +1,14 @@
-<?php
+
+<?php require ('partials/head.php') ?>
+
+    <?php require 'partials/nav.php' ?>
+    <?php require 'partials/banner.php' ?>
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 space-y-8">
+            <a href="/notes" class="text-blue-500 underline">back to notes ...</a>
+             <p> <?=  $note['body']  ?> </p>
+        </div>
+    </main>
+
+
+<?php require ('partials/footer.php') ?>
