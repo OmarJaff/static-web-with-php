49a5036b732198e50e5abac4b39183733e07f86d
OmarJaff <me@omarjaff.com>
2024-02-12 18:47:29 +0300

    Refactoring

--- Database.php
+++ Database.php
@@ -5,7 +5,7 @@
 class Database {
 
     public $connection;
-
+    protected $statement;
     public function __construct($config, $username = 'root', $password = '')
     {
 
@@ -17,10 +17,27 @@ class Database {
     }
     public function query($query, $params = [])
     {
-        $statement = $this->connection->prepare($query);
+        $this->statement = $this->connection->prepare($query);
 
-        $statement->execute($params);
+        $this->statement->execute($params);
 
-        return $statement;
-    }
+        return $this;
+     }
+
+     protected function find()
+     {
+         return $this->statement->fetch();
+     }
+
+     public function findOrFail()
+     {
+
+         $result = $this->find();
+
+         if(!$result) {
+             return abort();
+         }
+
+         return $result;
+     }
 }
