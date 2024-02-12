1c43db7486bd73db9ba4a20deef2a745e70570d8
OmarJaff <me@omarjaff.com>
2024-02-12 19:56:20 +0300

    Styling the form

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -5,9 +5,36 @@
 <?php require 'partials/banner.php' ?>
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 space-y-8">
-        <form action="">
-            create a note here
+
+        <form method="POST">
+            <div class="space-y-12">
+                <div class="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
+                    <div>
+                        <h2 class="text-base font-semibold leading-7 text-gray-900">Your Notes</h2>
+                        <p class="mt-1 text-sm leading-6 text-gray-600">This is where you can add a new note.</p>
+                    </div>
+
+                    <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
+                        <div class="col-span-full">
+                            <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Your Note</label>
+                            <div class="mt-2">
+                                <textarea placeholder="Write a new note here..." id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                            </div>
+                         </div>
+
+
+                     </div>
+                </div>
+
+
+             </div>
+
+            <div class="mt-6 flex items-center justify-end gap-x-6">
+                <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            </div>
         </form>
+
     </div>
 </main>
 
