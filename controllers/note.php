24c5689813b7e67defb05ca566381f83521d1ff3
OmarJaff <me@omarjaff.com>
2024-02-12 18:48:21 +0300

    Added controller

--- controllers/note.php
+++ controllers/note.php
@@ -8,16 +8,9 @@ $config = require ('config.php');
 $db = new Database($config['database']);
 
 
-$note = $db->query('select * from notes where id = :id', [':id' => $_GET['id']])->fetch();
+$note = $db->query('select * from notes where id = :id', [':id' => $_GET['id']])->findOrFail();
 
-if (!$note)
-{
-    abort();
-}
-
-if ($note['user_id'] !== 1) {
-    abort(Response::NOT_FOUND);
-}
+authorize($note['user_id'] === 1);
 
 require 'views/note.view.php';
 
