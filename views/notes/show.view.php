570ba4e32e9a90a5c06ce738ac15ff97e957663f
OmarJaff <me@omarjaff.com>
2024-02-13 21:57:03 +0300

    renaming file names

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -1,8 +1,8 @@
 
-<?php require('partials/head.php') ?>
+<?php require('views/partials/head.php') ?>
 
-    <?php require 'partials/nav.php' ?>
-    <?php require 'partials/banner.php' ?>
+    <?php require 'views/partials/nav.php' ?>
+    <?php require 'views/partials/banner.php' ?>
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 space-y-8">
             <a href="/notes" class="text-blue-500 underline">back to notes ...</a>
@@ -11,4 +11,4 @@
     </main>
 
 
-<?php require('partials/footer.php') ?>
+<?php require('views/partials/footer.php') ?>
