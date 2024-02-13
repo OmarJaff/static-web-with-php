0e1f198e3820618270c34a15e1a3c5be31dca8d8
OmarJaff <me@omarjaff.com>
2024-02-13 21:14:30 +0300

    refactored  note create

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,6 +1,7 @@
 <?php
 
 $config = require ('config.php');
+require 'Validator.php';
 $db = new Database($config['database']);
 
 $heading = "Create a note";
@@ -9,14 +10,12 @@ $heading = "Create a note";
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $errors = [];
-
-    if(strlen($_POST['body']) === 0) {
-        $errors['body'] = 'The body field is required';
+    $validator = new Validator();
+    if(! $validator->string($_POST['body'])) {
+        $errors['body'] = 'The body field that accept max of 1,000 characters is required';
     }
 
-    if(strlen($_POST['body']) > 1000) {
-        $errors['body'] = 'The body filed cannot be more than 1000 characters';
-    }
+
 
     if(empty($errors)) {
         $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', ['body' => $_POST['body'], 'user_id' => 1]);
