2ec17f7ccecdd920b1254a7251fd66acd588fd31
OmarJaff <me@omarjaff.com>
2024-02-13 20:56:58 +0300

    added validation to note-create

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -14,16 +14,23 @@
                         <p class="mt-1 text-sm leading-6 text-gray-600">This is where you can add a new note.</p>
                     </div>
 
-                    <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
+                    <div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6 md:col-span-2">
                         <div class="col-span-full">
                             <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Your Note</label>
                             <div class="mt-2">
-                                <textarea placeholder="Write a new note here..." id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                                <textarea placeholder="Write a new note here..." id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    <?=  isset($_POST['body']) ? $_POST['body'] :  '' ?>
+                                </textarea>
                             </div>
                          </div>
-
+                        <div class="col-span-full">
+                            <?php if(isset($errors['body'])) :?>
+                                <p class=" text-sm text-red-500"> <?= $errors['body'] ?> </p>
+                            <?php endif ?>
+                        </div>
 
                      </div>
+
                 </div>
 
 
