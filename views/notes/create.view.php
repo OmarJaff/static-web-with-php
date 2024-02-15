3319685c787e9453790d8d7a4f96de9ff698ddd5
OmarJaff <me@omarjaff.com>
2024-02-15 18:57:10 +0300

    used helper function for the base path

--- views/notes/create.view.php
+++ views/notes/create.view.php
@@ -1,8 +1,8 @@
 
-<?php require('views/partials/head.php') ?>
+<?php require base_path('views/partials/head.php') ?>
+<?php require base_path('views/partials/nav.php') ?>
+<?php require base_path('views/partials/banner.php') ?>
 
-<?php require 'views/partials/nav.php' ?>
-<?php require 'views/partials/banner.php' ?>
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 space-y-8">
 
@@ -44,4 +44,4 @@
 </main>
 
 
-<?php require('views/partials/footer.php') ?>
+<?php require base_path('views/partials/footer.php') ?>
