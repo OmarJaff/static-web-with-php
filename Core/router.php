dd1ea1e316baf336e7e6777ce75164dc263e00f0
OmarJaff <me@omarjaff.com>
2024-02-15 19:36:15 +0300

    grouped functions and router into Core folder

--- Core/router.php
+++ Core/router.php
@@ -3,12 +3,12 @@
 $url = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 
-$routes = require('routes.php');
+$routes = require base_path('routes.php');
 
   function routeToController($url, $routes)
 {
     if (array_key_exists($url, $routes)) {
-        require $routes[$url];
+        require base_path($routes[$url]);
     } else {
         abort();
     }
@@ -16,7 +16,7 @@ $routes = require('routes.php');
 function abort($code = 404)
 {
     http_response_code($code);
-    require "views/{$code}.view.php";
+    require base_path("views/{$code}.view.php");
 }
 
 routeToController($url, $routes);
