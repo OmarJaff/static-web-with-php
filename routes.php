4102c5a99cda3fc35799a44dc3a7b2f8c3a4ac2a
OmarJaff <me@omarjaff.com>
2024-02-16 20:41:43 +0300

    Added route for edit page

--- routes.php
+++ routes.php
@@ -8,6 +8,9 @@ $router->get('/contact', 'controllers/contact.php');
 $router->get('/notes', 'controllers/notes/index.php');
 $router->get('/note', 'controllers/notes/show.php');
 $router->get('/notes/create', 'controllers/notes/create.php');
+
 $router->post('/notes', 'controllers/notes/store.php');
+$router->get('/notes/edit', 'controllers/notes/edit.php');
+
 $router->delete('/note', 'controllers/notes/destroy.php');
 
