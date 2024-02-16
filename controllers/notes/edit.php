d70d7aac39feb5822ae32d4a000b73fe996d8636
OmarJaff <me@omarjaff.com>
2024-02-16 21:20:00 +0300

    Added update functionality to the project

--- controllers/notes/edit.php
+++ controllers/notes/edit.php
@@ -1,8 +1,20 @@
 <?php
 
 
+use Core\App;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+
+
+$note = $db->query('select * from notes where id = :id', [':id' => $_GET['id']])->findOrFail();
+
+
+authorize($note['user_id'] === 1);
+
 
 view("notes/edit.view.php", [
     'heading' => 'Edit a Note',
-    'errors' => []
+    'errors' => [],
+    'note' => $note
 ]);
--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -6,11 +6,12 @@ use Core\Database;
 
 $db = App::resolve(Database::class);
 
+$currentUserId = 1;
 
 $note = $db->query('select * from notes where id = :id', [':id' => $_GET['id']])->findOrFail();
 
 
-authorize($note['user_id'] === 1);
+authorize($note['user_id'] === $currentUserId);
 
 
 view("notes/show.view.php", [
