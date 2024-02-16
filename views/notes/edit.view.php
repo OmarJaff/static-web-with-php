054285d6ad370d40155a1e556ab50acb30c28bac
OmarJaff <me@omarjaff.com>
2024-02-16 21:19:03 +0300

    Added edit view for updating a note

--- views/notes/edit.view.php
+++ views/notes/edit.view.php
@@ -18,7 +18,7 @@
                         <div class="col-span-full">
                             <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Your Note</label>
                             <div class="mt-2">
-                                <textarea placeholder="Write a new note here..." id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"><?=  isset($_POST['body']) && isset($errors['body']) ?  trim($_POST['body']) : '' ?></textarea>
+                                <textarea placeholder="Write a new note here..." id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"><?=   $note['body']  ?></textarea>
                             </div>
                         </div>
                         <div class="col-span-full">
@@ -35,7 +35,21 @@
             </div>
 
             <div class="mt-6 flex items-center justify-end gap-x-6">
-                 <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+
+                <a href="/notes" type="submit" class="rounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Cancel</a>
+
+                <form  method="POST">
+                    <input type="hidden" name="_method" value="PATCH">
+                    <input type="hidden" name="id" value="<?= $note['id'] ?>">
+                    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+
+                </form>
+
+                <form  method="POST" action="/note">
+                    <input type="hidden" name="_method" value="DELETE">
+                    <input type="hidden" name="id" value="<?= $note['id'] ?>">
+                    <button class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Delete</button>
+                </form>
             </div>
         </form>
 
