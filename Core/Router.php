6d4e4a1671d960bb3ec46eb482636f666a571f1b
OmarJaff <me@omarjaff.com>
2024-02-16 17:05:17 +0300

    Fixed an error with using compact method

--- Core/Router.php
+++ Core/Router.php
@@ -7,47 +7,47 @@ class Router {
 
     protected $routes = [];
 
-    public function add($uri, $controller, $method) {
-            $this->routes[] = compact($uri, $controller, $method);
+    public function add($method,$uri, $controller) {
+            $this->routes[] = compact( 'method' ,'uri', 'controller');
        }
     public function get($uri, $controller)
     {
 
-      $this->add($uri, $controller, 'GET');
+      $this->add('GET',$uri, $controller);
 
     }
 
     public function post($uri, $controller)
     {
 
-        $this->add($uri, $controller, 'POST');
+        $this->add('POST' ,$uri, $controller);
 
     }
 
     public function delete($uri, $controller)
     {
 
-        $this->add($uri, $controller, 'DELETE');
+        $this->add('DELETE' ,$uri, $controller);
 
     }
 
     public function patch($uri, $controller)
     {
 
-        $this->add($uri, $controller, 'PATCH');
+        $this->add('PATCH',$uri, $controller);
 
     }
 
     public function put($uri, $controller)
     {
 
-        $this->add($uri, $controller, 'PUT');
+        $this->add('PUT' ,$uri, $controller);
 
     }
 
     public function route($uri, $method)
     {
-        foreach ($this->routes as $route) {
+         foreach ($this->routes as $route) {
             if($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
                 return require base_path($route['controller']);
             }
