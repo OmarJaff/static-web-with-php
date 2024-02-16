bccc7bdc34588a308e2ff674f33bb1f51ef1d63c
OmarJaff <me@omarjaff.com>
2024-02-16 16:09:24 +0300

    Added hidden methods for DELETE requests

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -5,6 +5,12 @@
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 space-y-8">
             <a href="/notes" class="text-blue-500 underline">back to notes ...</a>
              <p> <?=  htmlspecialchars($note['body'])  ?> </p>
+
+            <form class="mt-6" method="POST">
+                <input type="hidden" name="_method" value="DELETE">
+                <input type="hidden" name="id" value="<?= $note['id'] ?>">
+                <button class="text-sm text-red-500 ">Delete</button>
+            </form>
         </div>
     </main>
 
