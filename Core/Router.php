9786d19027bf80d7acabd99c469027a5605299d3
OmarJaff <me@omarjaff.com>
2024-02-16 16:42:38 +0300

    Added router logic by checking method and uri

--- Core/Router.php
+++ Core/Router.php
@@ -2,27 +2,6 @@
 
 namespace Core;
 
-//
-//$url = parse_url($_SERVER['REQUEST_URI'])['path'];
-//
-//
-//$routes = require base_path('routes.php');
-//
-//  function routeToController($url, $routes)
-//{
-//    if (array_key_exists($url, $routes)) {
-//        require base_path($routes[$url]);
-//    } else {
-//        abort();
-//    }
-//}
-//function abort($code = 404)
-//{
-//    http_response_code($code);
-//    require base_path("views/{$code}.view.php");
-//}
-//
-//routeToController($url, $routes);
 
 class Router {
 
@@ -65,4 +44,21 @@ class Router {
         $this->add($uri, $controller, 'PUT');
 
     }
+
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+                return require base_path($route['controller']);
+            }
+        }
+        $this->abort();
+    }
+
+   protected function abort($code = 404)
+{
+    http_response_code($code);
+    require base_path("views/{$code}.view.php");
+    die();
+}
 }
