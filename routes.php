902a81f0edc28fd2201aaf5d8eb0a1ac22c59eb4
OmarJaff <me@omarjaff.com>
2024-02-18 17:31:09 +0300

    Added auth and guest middlewares

--- routes.php
+++ routes.php
@@ -5,7 +5,7 @@ $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
 
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
 $router->get('/note', 'controllers/notes/show.php');
 $router->get('/notes/create', 'controllers/notes/create.php');
 
