4771fbbff5eb7f532c10138d0d054f17272c1849
OmarJaff <me@omarjaff.com>
2024-02-21 22:21:26 +0300

    Removed controllers in the router names

--- routes.php
+++ routes.php
@@ -1,25 +1,25 @@
 <?php
 
-$router->get('/', 'controllers/index.php');
-$router->get('/about', 'controllers/about.php');
-$router->get('/contact', 'controllers/contact.php');
+$router->get('/', 'index.php');
+$router->get('/about', 'about.php');
+$router->get('/contact', 'contact.php');
 
 
-$router->get('/notes', 'controllers/notes/index.php')->only('auth');
-$router->get('/note', 'controllers/notes/show.php');
-$router->get('/notes/create', 'controllers/notes/create.php');
+$router->get('/notes', 'notes/index.php')->only('auth');
+$router->get('/note', 'notes/show.php');
+$router->get('/notes/create', 'notes/create.php');
 
-$router->post('/notes', 'controllers/notes/store.php');
-$router->patch('/notes', 'controllers/notes/update.php');
+$router->post('/notes', 'notes/store.php');
+$router->patch('/notes', 'notes/update.php');
 
-$router->get('/notes/edit', 'controllers/notes/edit.php');
+$router->get('/notes/edit', 'notes/edit.php');
 
-$router->delete('/note', 'controllers/notes/destroy.php');
+$router->delete('/note', 'notes/destroy.php');
 
-$router->get('/register', 'controllers/registration/create.php')->only('guest');
-$router->post('/register', 'controllers/registration/store.php')->only('guest');
+$router->get('/register', 'registration/create.php')->only('guest');
+$router->post('/register', 'registration/store.php')->only('guest');
 
-$router->get('/login', 'controllers/sessions/create.php')->only('guest');
-$router->post('/sessions', 'controllers/sessions/store.php')->only('guest');
-$router->delete('/sessions', 'controllers/sessions/destroy.php')->only('auth');
+$router->get('/login', 'sessions/create.php')->only('guest');
+$router->post('/sessions', 'sessions/store.php')->only('guest');
+$router->delete('/sessions', 'sessions/destroy.php')->only('auth');
 
