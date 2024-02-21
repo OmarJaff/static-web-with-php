dd9e8684b10f05108c9cc57f1afc9a66d4dce83e
OmarJaff <me@omarjaff.com>
2024-02-21 22:47:10 +0300

    Created LoginForm class

--- http/forms/LoginForm.php
+++ http/forms/LoginForm.php
@@ -1,8 +1,30 @@
 <?php
 
-namespace forms;
+namespace http\forms;
+
+use Core\Validator;
 
 class LoginForm
 {
+    protected array $errors = [];
 
+    public function validate($email, $password): bool
+    {
+
+        if(! Validator::email($email)) {
+           $this->errors['email'] = "Current credentials are incorrect.";
+        }
+
+        if(! Validator::string($password, 7, 255)) {
+            $this->errors['password'] = "Current credentials are incorrect.";
+        }
+
+        return empty($this->errors);
+
+    }
+
+    public function errors(): array
+    {
+        return $this->errors;
+    }
 }
