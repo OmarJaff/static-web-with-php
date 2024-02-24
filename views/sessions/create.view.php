68c6f1f964f581f45104089ef7fda56221707b56
OmarJaff <me@omarjaff.com>
2024-02-24 19:03:42 +0300

    Used old() helper function

--- views/sessions/create.view.php
+++ views/sessions/create.view.php
@@ -14,7 +14,9 @@
                     <div class="pointer-events-none absolute inset-0 z-10 rounded-md ring-1 ring-inset ring-gray-300"></div>
                     <div>
                         <label for="email" class="sr-only">Email address</label>
-                        <input id="email" name="email" type="email" autocomplete="email" required class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email address">
+                        <input id="email" name="email" type="email" autocomplete="email" required
+                               value="<?= old('email') ?? '' ?>"
+                               class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email address">
                     </div>
                     <div>
                         <label for="password" class="sr-only">Password</label>
