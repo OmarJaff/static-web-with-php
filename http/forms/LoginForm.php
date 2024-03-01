776944b8afbf701588de44dfb96a3e24e688b367
OmarJaff <me@omarjaff.com>
2024-03-01 23:15:04 +0300

    Refactored

--- http/forms/LoginForm.php
+++ http/forms/LoginForm.php
@@ -8,19 +8,30 @@ class LoginForm
 {
     protected array $errors = [];
 
-    public function validate($email, $password): bool
+    public function __construct ($attributes)
     {
-
-        if(! Validator::email($email)) {
-           $this->errors['email'] = "Current credentials are incorrect.";
+        if(! Validator::email($attributes['email'])) {
+            $this->errors['email'] = "Current credentials are incorrect.";
         }
 
-        if(! Validator::string($password, 7, 255)) {
+        if(! Validator::string($attributes['password'], 7, 255)) {
             $this->errors['password'] = "Current credentials are incorrect.";
         }
+    }
 
-        return empty($this->errors);
+    public static function  validate($attributes): bool
+    {
 
+        $instance = new static($attributes);
+
+        if($this->failed()) {
+
+        }
+    }
+
+    public function failed (): int
+    {
+        return count($this->errors);
     }
 
     public function errors(): array
