2e63aa5be9081851a2f0823bdb34b018bcdaae2e
OmarJaff <me@omarjaff.com>
2024-03-04 12:19:11 +0300

    Made the redirect line more readable

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Core\Session;
+use Core\ValidationException;
 
 session_start();
 
@@ -26,7 +27,18 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
 
+try {
 
-$router->route($uri, $method);
+    $router->route($uri, $method);
+
+} catch (ValidationException $exception ) {
+
+    Session::flash('errors', $exception->errors);
+
+    Session::flash('old', $exception->old);
+
+    return redirect($router->previousURL());
+}
+
 
 Session::unflash();
